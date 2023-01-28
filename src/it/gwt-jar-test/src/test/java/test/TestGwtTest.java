f406f06bdb42007fd983589e8d6cb19ebd217e39
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2023-01-28 17:18:55 +1100

    JSON.gwt.xml

---
+++ src/it/gwt-jar-test/src/test/java/test/TestGwtTest.java
@@ -0,0 +1,72 @@
+package test;
+
+import com.google.gwt.junit.client.GWTTestCase;
+
+import walkingkooka.net.Url;
+import walkingkooka.net.header.CharsetName;
+import walkingkooka.net.header.HttpHeaderName;
+import walkingkooka.net.header.MediaType;
+import walkingkooka.net.http.HttpEntity;
+import walkingkooka.net.http.HttpProtocolVersion;
+import walkingkooka.net.http.HttpStatusCode;
+import walkingkooka.net.http.HttpTransport;
+import walkingkooka.net.http.json.JsonHttpRequestHttpResponseBiConsumers;
+import walkingkooka.net.http.server.HttpRequest;
+import walkingkooka.net.http.server.HttpRequests;
+import walkingkooka.net.http.server.HttpResponse;
+import walkingkooka.net.http.server.HttpResponses;
+import walkingkooka.tree.json.JsonNode;
+
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+
+public class TestGwtTest extends GWTTestCase {
+
+    @Override
+    public String getModuleName() {
+        return "test.Test";
+    }
+
+    public void testAssertEquals() {
+        assertEquals(
+                1,
+                1
+        );
+    }
+
+    public void testSuccessful() throws Exception {
+        final JsonNode in = JsonNode.number(1);
+        final JsonNode out = JsonNode.number(2);
+        final BiConsumer<HttpRequest, HttpResponse> consumer = JsonHttpRequestHttpResponseBiConsumers.json(
+                (json) -> out,
+                Function.identity()
+        );
+
+        final HttpRequest request = HttpRequests.post(HttpTransport.UNSECURED,
+                Url.parseRelative("/handler"),
+                HttpProtocolVersion.VERSION_1_0,
+                HttpEntity.EMPTY
+                        .addHeader(HttpHeaderName.CONTENT_TYPE, MediaType.APPLICATION_JSON)
+                        .addHeader(HttpHeaderName.ACCEPT, MediaType.APPLICATION_JSON.accept())
+                        .setBodyText(in.toString())
+                        .setContentLength());
+
+        final HttpResponse response = HttpResponses.recording();
+
+        consumer.accept(request, response);
+
+        final String responseBody = out.toString();
+
+        final HttpResponse expected = HttpResponses.recording();
+        expected.setStatus(HttpStatusCode.OK.status());
+        expected.addEntity(HttpEntity.EMPTY
+                .addHeader(HttpHeaderName.CONTENT_TYPE, MediaType.APPLICATION_JSON.setCharset(CharsetName.UTF_8))
+                .setBodyText(responseBody)
+                .setContentLength());
+
+        assertEquals(
+                expected,
+                response
+        );
+    }
+}
